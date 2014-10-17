a194834c293fd6dcfcc5bf26c8195b8436537688
sergey kalinin <core.editor>
2014-10-17 19:16:42 +0400

    templatetag modal compl

--- app_zapsum/templatetags/tags_app_zapsum.py
+++ app_zapsum/templatetags/tags_app_zapsum.py
@@ -14,7 +14,31 @@ def part_pagination(entries, list_pages, last_page, first_page):
 		'first_page': first_page,
 	}	
 	
+@register.inclusion_tag("part_modal.html")
+def part_modal(modal, action):
+	window_type = None
+	window_type_label = None
+	timeout = None
+	message = None
 
+	for entry in modal:
+		if action == entry.id:
+			if entry.window == 0:
+				window_type = 'infoModal'
+				window_type_label = 'mySmallModalLabel'
+			elif entry.window == 1:	
+				window_type = 'commonModal'
+				window_type_label = 'commonModalLabel'					
+
+			timeout = entry.timeout	
+			message = entry.message
+
+	return {
+		'window_type': window_type,
+		'window_type_label': window_type_label,
+		'timeout': timeout,
+		'message': message,
+	}
 	
 
 
